0ba3eea096df989502b73411c75878edefe4a32b
DBARANOV <dmitry-baranov>
2018-05-23 15:39:28 +0300

    4 commit

--- src/main/java/errors/Response.java
+++ src/main/java/errors/Response.java
@@ -3,7 +3,10 @@ package errors;
 public enum Response {
 
     FILE_NOT_FOUND(1001, "Файл отсутствует!"),
-    INVALID_INPUT_DATA(1001, "Входыне данные некорректны");
+    INVALID_INPUT_DATA(1002, "Входыне данные некорректны!"),
+    RESULT_FILE_NOT_FOUND(1003, "Результирующий файл не найден!"),
+    UNSUPPORTED_ENCODING_EXCEPTION(1004, "Ошибка кодирования результирующего файла!"),
+    NUBER_FORMAT_EXCEPTION(1005, "Неверно указаны данные в исходном файле!");
 
     private int errorCode;
     private String errorMessage;
--- src/main/java/uInteraction/ReadWriteFile.java
+++ src/main/java/uInteraction/ReadWriteFile.java
@@ -5,6 +5,7 @@ import data.Employee;
 import errors.MyException;
 import errors.Response;
 import logic.Util;
+
 import java.io.*;
 import java.math.BigInteger;
 import java.util.*;
@@ -23,13 +24,14 @@ public class ReadWriteFile {
             readFile();
             writeFile();
         } catch (MyException e) {
-            System.out.println(e.getResponse().getErrorCode() + e.getResponse().getErrorMessage());
+            System.out.println(e.getResponse().getErrorCode() + " " + e.getResponse().getErrorMessage());
         }
-
     }
 
-    private void readFile() throws NumberFormatException {
-        try (BufferedReader br = new BufferedReader(new FileReader(FILENAMEREAD))) {
+    private void readFile() {
+        try {
+            InputStream inputStream = new FileInputStream(FILENAMEREAD);
+            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
             String sCurrentLine;
             while ((sCurrentLine = br.readLine()) != null) {
                 String[] s = sCurrentLine.split(";");
@@ -38,34 +40,56 @@ public class ReadWriteFile {
                 }
                 departments.get(s[2]).getEmployeeList().add(new Employee(s[0], s[1], new BigInteger(s[3])));
             }
+        } catch (NumberFormatException e) {
+            throw new MyException(Response.NUBER_FORMAT_EXCEPTION);
+        } catch (UnsupportedEncodingException e) {
+            throw new MyException(Response.INVALID_INPUT_DATA);
         } catch (IOException e) {
             throw new MyException(Response.FILE_NOT_FOUND);
-        } catch (NumberFormatException e) {
-            throw new MyException(Response.INVALID_INPUT_DATA);
         }
     }
+
+//    private void readFile() {
+//        try (BufferedReader br = new BufferedReader(new FileReader(FILENAMEREAD))) {
+//            String sCurrentLine;
+//            while ((sCurrentLine = br.readLine()) != null) {
+//                String[] s = sCurrentLine.split(";");
+//                if (!departments.containsKey(s[2])) {
+//                    departments.put(s[2], new Department());
+//                }
+//                departments.get(s[2]).getEmployeeList().add(new Employee(s[0], s[1], new BigInteger(s[3])));
+//            }
+//        } catch (IOException e) {
+//            throw new MyException(Response.FILE_NOT_FOUND);
+//        } catch (NumberFormatException e) {
+//            throw new MyException(Response.INVALID_INPUT_DATA);
+//        }
+//    }
 
     private void writeFile() {
         PrintWriter writer = null;
         try {
             writer = new PrintWriter(FILENAMEWRITE, "UTF-8");
-        } catch (FileNotFoundException | UnsupportedEncodingException e) {
-            System.out.println("Результрующий файл не найден");
+        } catch (FileNotFoundException e) {
+            throw new MyException(Response.RESULT_FILE_NOT_FOUND);
+        } catch (UnsupportedEncodingException e) {
+            throw new MyException(Response.UNSUPPORTED_ENCODING_EXCEPTION);
         }
         for (Map.Entry<String, Department> departmentEntryFrom : departments.entrySet()) {
             List<Employee> fromList = departmentEntryFrom.getValue().getEmployeeList();
-            BigInteger ofAvgSalary = Util.avgSalary(fromList);
-            writer.println("From department " + departmentEntryFrom.getKey());
+            BigInteger fromAvgSalary = Util.avgSalary(fromList);
+            writer.println("Из отдела " + departmentEntryFrom.getKey());
             for (Map.Entry<String, Department> departmentEntryTo : departments.entrySet()) {
                 if (!departmentEntryFrom.getKey().equals(departmentEntryTo.getKey())) {
                     List<Employee> toTheList = departmentEntryTo.getValue().getEmployeeList();
                     BigInteger toTheAvgSalary = Util.avgSalary(toTheList);
-                    writer.print("\nTo the department " + departmentEntryTo.getKey() + ":");
+                    writer.print("\nВ отдел " + departmentEntryTo.getKey() + ":");
                     List<List<Employee>> result = Util.combination(fromList.size() - 1, new ArrayList<>(), fromList);
                     if (result.size() != 0) {
                         for (List<Employee> listCombination : result) {
-                            if (Util.comparisonSalary(ofAvgSalary, toTheAvgSalary, listCombination)) {
+                            if (Util.comparisonSalary(fromAvgSalary, toTheAvgSalary, listCombination)) {
                                 writer.print("\n");
+
                                 for (Employee e : listCombination) {
                                     writer.print(" Name: " + e.getName() + ", Surname: " + e.getSurname() + ", "
                                             + "Salary: " + e.getSalary().toString() + ";");
